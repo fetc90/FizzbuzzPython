76ad3a915cd8c824d9b886e2783d15e50924d4c1
Francesca Chater <francescachater@gmail.com>
2019-07-09 11:21:19 +0100

    Fizzbuzz test framework passing
    Co-authored-by: Ben Reynolds-Carr <bjr.kitz@gmail.com>

---
+++ spec/test_fizzbuzz.py
@@ -0,0 +1,15 @@
+# from ..lib import fizzbuzz
+# import lib.fizzbuzz
+from lib.fizzbuzz import fizzbuzz
+
+def test_fizz():
+    assert fizzbuzz(3) == 'fizz'
+    assert fizzbuzz(1) == 1
+    assert fizzbuzz(6) == 'fizz'
+
+def test_buzz():
+    assert fizzbuzz(5) == 'buzz'
+    assert fizzbuzz(10) == 'buzz'
+
+def test_fizzbuzz():
+    assert fizzbuzz(15) == 'fizzbuzz'
